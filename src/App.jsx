1305b32d486241ef2a0edeea50d25dfc70f266fd
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-04-03 00:45:27 -0400

    complete game

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,61 @@
+import { useState } from 'react'
 import React from "react";
 import "./App.css";
 
 function App () {
+  const [ score, setScore ] = useState(0)
+  const [ incrementor, setIncrementor] = useState("+1")
+  const [ x, setX ] = useState(1)
+  const [ y, setY ] = useState(2)
+  const [ msg, setMsg ] = useState("none")
+  const [ game, setGame ] =useState("block")
+
+
+  function changeScore() {
+    if (score >= 100) {
+      setGame("none")
+      setMsg("block")
+    }
+    if (incrementor === `+${y}`) {
+      setScore(score + y)
+    } else {
+      setScore(score + x)
+    }
+  }
+
+  function changeIncrementor () {
+    if (score >= 10 ) {
+      setIncrementor(`+${y}`)
+      setScore(score - 10)
+      setX(y)
+      setY(y + 1)
+    } else {
+      alert("You can't afford that!")
+    }
+  }
+
+  function restartGame() {
+    setMsg("none")
+    setGame("block")
+    setScore(0)
+    setIncrementor("+1")
+    setX(1)
+    setY(2)
+  }
+
     return (
-      <main>
-        <p>React State Lab</p>
+      <main className="App">
+        <h1>Current Score: {score}</h1>
+        <div className="clicker" style={{display: game}}>
+        <button onClick={changeScore}>{incrementor}</button>
+        <br />
+        <br />
+        <button onClick={changeIncrementor}>Pay 10 points to change from +{x} to +{y}</button>
+        </div>
+        <div className="winMsg" style={{display: msg}}>
+          <h2>You Win!</h2>
+          <button onClick={restartGame}>Play again?</button>
+        </div>
       </main>
     );
 }
