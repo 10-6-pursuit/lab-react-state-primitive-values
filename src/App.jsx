175d46e4e66a244069e644804966bf2a96e48eb9
alexandra-perez <alexandraperezny@gmail.com>
2024-04-21 13:03:30 -0400

    Fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
+import Main from "./Components/Main";
 
-function App () {
+function App() {
     return (
-      <main>
-        <p>React State Lab</p>
-      </main>
+      <Main/>
     );
 }
 
